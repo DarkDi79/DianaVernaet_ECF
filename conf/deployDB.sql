84afe46f09303f6291fbd36d932617410c8cb953
DarkDi79 <diana.vernaet@gmail.com>
2022-11-01 14:50:25 +0100

    retrait insert

--- conf/deployDB.sql
+++ conf/deployDB.sql
@@ -40,19 +40,6 @@ CREATE TABLE `centers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `centers`
---
-
-INSERT INTO `centers` (`ctr_id`, `ctr_name`, `ctr_usr_id`, `ctr_mail`, `ctr_opt_towel`, `ctr_opt_drinks`, `ctr_opt_planning`, `ctr_opt_food`, `ctr_active`) VALUES
-(1, 'Alesia', 4, 'alesia-paris14@mail.com', 0, 0, 1, 1, 0),
-(3, 'la vilette', 16, 'pierre@mail.com', 0, 0, 0, 1, 1),
-(4, 'La Pierre', 30, 'diana@mail.com', 0, 0, 0, 1, 1),
-(5, 'Le Rocher', 16, 'contact@lerocher.com', 1, 0, 1, 0, 1),
-(6, 'Denfert', 30, 'contact@denfert.com', 1, 0, 1, 0, 0);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `partners`
 --
 
@@ -68,16 +55,6 @@ CREATE TABLE `partners` (
   `ptr_active` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `partners`
---
-
-INSERT INTO `partners` (`ptr_id`, `ptr_name`, `ptr_mail`, `ptr_usr_id`, `ptr_opt_towel`, `ptr_opt_drinks`, `ptr_opt_planning`, `ptr_opt_food`, `ptr_active`) VALUES
-(1, 'Paris 14', 'paris14@mail.com', 2, 1, 0, 1, 1, 1),
-(10, 'Super Franchise', 'moi@superfranchise.com', 27, 0, 0, 0, 1, 1),
-(11, 'Dark Side', 'diana@mail.com', 26, 1, 1, 1, 1, 1);
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `ptr_to_ctr`
@@ -89,17 +66,7 @@ CREATE TABLE `ptr_to_ctr` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `ptr_to_ctr`
---
 
-INSERT INTO `ptr_to_ctr` (`ptc_ptr_id`, `ptc_ctr_id`) VALUES
-(1, 1),
-(1, 6),
-(11, 5);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `roles`
 --
 
@@ -131,25 +98,6 @@ CREATE TABLE `users` (
   `usr_pw` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `usr_change_pw` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`usr_id`, `usr_name`, `usr_rol_id`, `usr_mail`, `usr_pw`, `usr_change_pw`) VALUES
-(2, 'Pierre Dupont', 'PART', 'pierredupont@mail.com', 'motdepasse', 0),
-(3, 'Paul Durand', 'PART', 'pauldurand@mail.com', 'testmodifPWD', 0),
-(4, 'Joe Lille', 'CENT', 'joelille@mail.com', 'nouveaumdp', 0),
-(16, 'Pierre', 'CENT', 'pierre@mail.com', 'motdepasse', 1),
-(25, 'Ody', 'TECH', 'Odyssea@mail.com', '$2y$10$jbK.Ftl23psQQog0UsPlGunzK.kKLBXK79QifkJ6fwRjziqcnxhue', 0),
-(26, 'Diana', 'PART', 'diana@mail.com', '$2y$10$H3c1LFuBQuyWFpn/u0aoN.9k21mnH1pLXs64xuImBOzGo8eIoZDGW', 1),
-(27, 'toto', 'PART', 'toto@toto.com', '$2y$10$Hxoj4Kv4ehOyOWTEDr963.XD37NX0bEGoRtkFctTRBOkTdIIHR5Jq', 1),
-(28, 'tata', 'CENT', 'tata@tata.com', '$2y$10$AfDIWEOJCc.JKm.Ksk7wwOA3Lt/xNz3Ke/1GNREw1K565jnwiFdEm', 1),
-(29, 'Luis', 'PART', 'luis@mail.com', '$2y$10$BPLCDULcjLL5rYz12/RmlumMJiHGFP4Q2Du2dpjPhUBDrV8sfLGFm', 1),
-(30, 'Greg', 'CENT', 'greg@mail.com', '$2y$10$pZIDnEnFLrbALZEVblIE9OcypTLjOg8ydnxBm4t6Q/YKZXC/R0DEK', 1),
-(32, 'moi', 'PART', 'moi@gmail.com', '$2y$10$R8FuGjkjBhz70pNyS3NsDueADlTWO7h04r/9DxtdcR.NeuDjWVlt.', 1),
-(33, 'coucou', 'PART', 'coucou@gmail.com', '$2y$10$BPyqgA31etDVqn.kO8fnWeN/C0gy/t15pMO2bE/.h8V7ODiaeFNqS', 1),
-(34, 'lui', 'PART', 'lui@gmail.com', '$2y$10$yY2q1n3iw1rrl/HzfJySeOUpt9x.TSpxYJyVxgBWBluEtJ3Wbc2sq', 0);
 
 --
 -- Indexes for dumped tables
